6677881859eb470f980671405486593a37b78490
Jonathan Powell <jonpowell2@icloud.com>
2020-02-09 10:27:23 +0000

    Change API Title on Swagger. Namespacing on Repo.

--- Deloitte.Scenario.Data/CityRepository.cs
+++ Deloitte.Scenario.Data/CityRepository.cs
@@ -12,72 +12,72 @@ using System.Threading.Tasks;
 namespace Deloitte.Scenario.Data
 {
 
+    public class CityRepository : ICityRepository
+    {
+        private readonly ICityContext _cityContext;
+        private readonly IMapper _mapper;
+
+        public CityRepository(ICityContext cityContext, IMapper mapper)
+        {
+            _cityContext = cityContext ?? throw new ArgumentNullException(nameof(cityContext));
+            _mapper = mapper ?? throw new ArgumentNullException(nameof(mapper));
+        }
+
+        public async Task<IEnumerable<City>> GetCityByNameAsync(string name)
+        {
+            var cities = _mapper.Map<IEnumerable<CityEntity>, IEnumerable<City>>(await _cityContext.Cities
+                                                                           .Where(c => c.Name.ToLower() == name.ToLower())
+                                                                           .ToListAsync());
+            return cities;
+        }
+
+        public async Task<int> AddCityAsync(City city)
+        {
+            var cityEntity = _mapper.Map<City, CityEntity>(city);
+
+            await _cityContext.Cities.AddAsync(cityEntity);
+
+            await _cityContext.SaveChangesAsync();
+
+            return cityEntity.Id;
+        }
+
+        public async Task<bool> DeleteCityAsync(int id)
+        {
+            var cityEntity = await _cityContext.Cities.Where(c => c.Id == id).FirstOrDefaultAsync();
+
+            if (cityEntity == null)
+                return false;
+
+            _cityContext.Cities.Remove(cityEntity);
+
+            await _cityContext.SaveChangesAsync();
+
+            return true;
+        }
+
+        public async Task<bool> UpdateCityAsync(int id, City city)
+        {
+            var cityEntity = await _cityContext.Cities.Where(c => c.Id == id).FirstOrDefaultAsync();
+
+            cityEntity.EstimatedPopulation = city.EstimatedPopulation;
+            cityEntity.DateEstablished = city.DateEstablished;
+            cityEntity.TouristRating = city.TouristRating;
+
+            _cityContext.Cities.Update(cityEntity);
+
+            await _cityContext.SaveChangesAsync();
+
+            return true;
+        }
+
+        public async Task<City> GetCityByIdAsync(int id)
+        {
+            var city = _mapper.Map<CityEntity, City>(await _cityContext.Cities.Where(c => c.Id == id).FirstOrDefaultAsync());
+
+            return city;
+        }
+
+    }
+
 }
-public class CityRepository : ICityRepository
-{
-    private readonly ICityContext _cityContext;
-    private readonly IMapper _mapper;
-
-    public CityRepository(ICityContext cityContext, IMapper mapper)
-    {
-        _cityContext = cityContext ?? throw new ArgumentNullException(nameof(cityContext));
-        _mapper = mapper ?? throw new ArgumentNullException(nameof(mapper));
-    }
-
-    public async Task<IEnumerable<City>> GetCityByNameAsync(string name)
-    {
-        var cities = _mapper.Map<IEnumerable<CityEntity>, IEnumerable<City>>(await _cityContext.Cities
-                                                                       .Where(c => c.Name.ToLower() == name.ToLower())
-                                                                       .ToListAsync());
-        return cities;
-    }
-
-    public async Task<int> AddCityAsync(City city)
-    {
-        var cityEntity = _mapper.Map<City, CityEntity>(city);
-
-        await _cityContext.Cities.AddAsync(cityEntity);
-
-        await _cityContext.SaveChangesAsync();
-
-        return cityEntity.Id;
-    }
-
-    public async Task<bool> DeleteCityAsync(int id)
-    {
-        var cityEntity = await _cityContext.Cities.Where(c => c.Id == id).FirstOrDefaultAsync();
-
-        if (cityEntity == null)
-            return false;
-
-        _cityContext.Cities.Remove(cityEntity);
-
-        await _cityContext.SaveChangesAsync();
-
-        return true;
-    }
-
-    public async Task<bool> UpdateCityAsync(int id, City city)
-    {
-        var cityEntity = await _cityContext.Cities.Where(c => c.Id == id).FirstOrDefaultAsync();
-
-        cityEntity.EstimatedPopulation = city.EstimatedPopulation;
-        cityEntity.DateEstablished = city.DateEstablished;
-        cityEntity.TouristRating = city.TouristRating;
-
-        _cityContext.Cities.Update(cityEntity);
-
-        await _cityContext.SaveChangesAsync();
-
-        return true;
-    }
-
-    public async Task<City> GetCityByIdAsync(int id)
-    {
-        var city = _mapper.Map<CityEntity, City>(await _cityContext.Cities.Where(c => c.Id == id).FirstOrDefaultAsync());
-
-        return city;
-    }
-
-}
-
